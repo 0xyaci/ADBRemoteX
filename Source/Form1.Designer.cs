9887f0fc4627b05cb568faba449f33cb9ca99b32
yaس <yurtsuki@gmail.com>
2025-11-01 19:40:50 +0100

    Uploaded the source code

---
+++ Source/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace ADBRemoteX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.btnVolUp = new System.Windows.Forms.Button();
+            this.btnVolDown = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnUp
+            // 
+            this.btnUp.BackColor = System.Drawing.Color.White;
+            this.btnUp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.33962F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUp.Location = new System.Drawing.Point(135, 143);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(76, 63);
+            this.btnUp.TabIndex = 0;
+            this.btnUp.Text = "↑";
+            this.btnUp.UseVisualStyleBackColor = false;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.BackColor = System.Drawing.Color.White;
+            this.btnRight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.33962F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRight.Location = new System.Drawing.Point(210, 212);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(76, 63);
+            this.btnRight.TabIndex = 1;
+            this.btnRight.Text = "→";
+            this.btnRight.UseVisualStyleBackColor = false;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click_1);
+            // 
+            // btnDown
+            // 
+            this.btnDown.BackColor = System.Drawing.Color.White;
+            this.btnDown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.33962F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDown.Location = new System.Drawing.Point(135, 283);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(76, 63);
+            this.btnDown.TabIndex = 2;
+            this.btnDown.Text = "↓";
+            this.btnDown.UseVisualStyleBackColor = false;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click_1);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.BackColor = System.Drawing.Color.White;
+            this.btnLeft.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.33962F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLeft.Location = new System.Drawing.Point(58, 212);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(76, 63);
+            this.btnLeft.TabIndex = 3;
+            this.btnLeft.Text = "←";
+            this.btnLeft.UseVisualStyleBackColor = false;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click_1);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.BackColor = System.Drawing.Color.White;
+            this.btnEnter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.33962F, System.Drawing.FontStyle.Bold);
+            this.btnEnter.Location = new System.Drawing.Point(149, 221);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(46, 44);
+            this.btnEnter.TabIndex = 4;
+            this.btnEnter.Text = "ok";
+            this.btnEnter.UseVisualStyleBackColor = false;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Segoe UI Semibold", 8.150944F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(239, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(76, 35);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "connect";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btnVolUp
+            // 
+            this.btnVolUp.BackColor = System.Drawing.Color.White;
+            this.btnVolUp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVolUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.33962F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVolUp.Location = new System.Drawing.Point(47, 359);
+            this.btnVolUp.Name = "btnVolUp";
+            this.btnVolUp.Size = new System.Drawing.Size(76, 36);
+            this.btnVolUp.TabIndex = 6;
+            this.btnVolUp.Text = "+";
+            this.btnVolUp.UseVisualStyleBackColor = false;
+            this.btnVolUp.Click += new System.EventHandler(this.btnVolUp_Click_1);
+            // 
+            // btnVolDown
+            // 
+            this.btnVolDown.BackColor = System.Drawing.Color.White;
+            this.btnVolDown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVolDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.33962F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVolDown.Location = new System.Drawing.Point(224, 359);
+            this.btnVolDown.Name = "btnVolDown";
+            this.btnVolDown.Size = new System.Drawing.Size(76, 36);
+            this.btnVolDown.TabIndex = 7;
+            this.btnVolDown.Text = "–";
+            this.btnVolDown.UseVisualStyleBackColor = false;
+            this.btnVolDown.Click += new System.EventHandler(this.btnVolDown_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.339622F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(239, 507);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 36);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "back";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.339622F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(157, 507);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 36);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "home";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtIP.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtIP.Font = new System.Drawing.Font("Segoe UI", 10.18868F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIP.ForeColor = System.Drawing.Color.Black;
+            this.txtIP.Location = new System.Drawing.Point(29, 20);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(148, 27);
+            this.txtIP.TabIndex = 10;
+            this.txtIP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtIP.TextChanged += new System.EventHandler(this.txtIP_TextChanged);
+            this.txtIP.Enter += new System.EventHandler(this.txtIP_Enter_1);
+            this.txtIP.Leave += new System.EventHandler(this.txtIP_Leave_1);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Arial", 10.83019F, System.Drawing.FontStyle.Bold);
+            this.lblStatus.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lblStatus.Location = new System.Drawing.Point(25, 60);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(144, 19);
+            this.lblStatus.TabIndex = 11;
+            this.lblStatus.Text = "Not Connected :((";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(65)))), ((int)(((byte)(42)))));
+            this.ClientSize = new System.Drawing.Size(352, 707);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnVolDown);
+            this.Controls.Add(this.btnVolUp);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnUp);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ADBRemoteX";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnVolUp;
+        private System.Windows.Forms.Button btnVolDown;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Source/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADBRemoteX
+{
+    public partial class Form1 : Form
+    {
+        private bool IsValidIP(string ip)
+        {
+            string pattern = @"^(\d{1,3}\.){3}\d{1,3}$";
+            return Regex.IsMatch(ip, pattern);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            txtIP.Text = "192.168.x.xxx";
+            txtIP.ForeColor = Color.Gray;
+        }
+        private void RunADB(string args)
+        {
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = "adb.exe",
+                    Arguments = args,
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true
+                };
+
+                Process proc = Process.Start(psi);
+                string output = proc.StandardOutput.ReadToEnd();
+                string error = proc.StandardError.ReadToEnd();
+                proc.WaitForExit();
+
+                Debug.WriteLine($"ADB Output: {output}");
+                Debug.WriteLine($"ADB Error: {error}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ADB error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrWhiteSpace(Properties.Settings.Default.LastIP))
+            {
+                txtIP.Text = Properties.Settings.Default.LastIP;
+                txtIP.ForeColor = Color.Black;
+            }
+
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            RunADB("shell input keyevent 19");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!IsValidIP(txtIP.Text))
+            {
+                MessageBox.Show("Invalid IP address format!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Properties.Settings.Default.LastIP = txtIP.Text;
+            Properties.Settings.Default.Save();
+            RunADB($"connect {txtIP.Text}:5555");
+            lblStatus.Text = "Connected :33";
+            lblStatus.ForeColor = Color.Green;
+
+        }
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RunADB("shell input keyevent 2");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RunADB("shell input keyevent 4");
+        }
+
+        private void btnVolUp_Click_1(object sender, EventArgs e)
+        {
+            RunADB("shell input keyevent 24");
+
+        }
+
+        private void btnVolDown_Click_1(object sender, EventArgs e)
+        {
+            RunADB("shell input keyevent 25");
+
+        }
+
+        private void txtIP_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void btnEnter_Click_1(object sender, EventArgs e)
+        {
+            RunADB("shell input keyevent 66");
+        }
+
+        private void btnRight_Click_1(object sender, EventArgs e)
+        {
+            RunADB("shell input keyevent 22");
+        }
+
+        private void btnLeft_Click_1(object sender, EventArgs e)
+        {
+            RunADB("shell input keyevent 21");
+        }
+
+        private void btnDown_Click_1(object sender, EventArgs e)
+        {
+            RunADB("shell input keyevent 20");
+        }
+
+        private void txtIP_Enter_1(object sender, EventArgs e)
+        {
+
+            if (txtIP.ForeColor == Color.Gray)
+            {
+                txtIP.Text = "";
+                txtIP.ForeColor = Color.Black;
+            }
+
+
+        }
+
+        private void txtIP_Leave_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtIP.Text))
+            {
+                txtIP.ForeColor = Color.Gray;
+                txtIP.Text = "192.168.x.xxx";
+            }
+        }
+    }
+}
